955b005e2559ccdb9caabde1dd8050afd5e7b96e
Scott J. Lipcon <slipcon@gmail.com>
2017-08-16 22:53:36 -0400

    workstation changes

--- install.sh
+++ install.sh
@@ -9,3 +9,16 @@ for i in local-* ;  do
 	chmod 644 $TARGET/$i
 done
 	
+cd ..
+
+TARGET=/etc/logcheck/ignore.d.workstation
+SOURCE=ignore.d.workstation
+
+cd $SOURCE
+for i in local-* ;  do
+	cp $i $TARGET 
+	chown root:logcheck $TARGET/$i
+	chmod 644 $TARGET/$i
+done
+	
+cd ..
